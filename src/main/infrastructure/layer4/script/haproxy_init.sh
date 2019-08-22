64d5af4c9601aa059ed1a10700ff07068a6062cd
rvcoutinho <me@rvcoutinho.com>
2019-08-21 22:58:28 -0300

    - Updating cache;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

---
+++ src/main/infrastructure/layer4/script/haproxy_init.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Default script behavior.
+set -o errexit
+#set -o pipefail
+
+# Default paramterers.
+DEBUG=false
+DEBUG_OPT=
+HAPROXY_PARAMETERS=
+
+# For each argument.
+while :; do
+	case ${1} in
+		
+		# Debug argument.
+		--debug)
+			DEBUG=true
+			DEBUG_OPT="--debug"
+			;;
+
+		# Other option.
+		?*)
+			HAPROXY_PARAMETERS="${HAPROXY_PARAMETERS} ${1}"
+			;;
+
+		# No more options.
+		*)
+			break
+
+	esac 
+	shift
+done
+
+# Using unavaialble variables should fail the script.
+set -o nounset
+
+# Enables interruption signal handling.
+trap - INT TERM
+
+# Print arguments if on debug mode.
+${DEBUG} && echo  "Running 'haproxy_init'"
+
+# Executes the init script.
+${DEBUG} && echo "exec /docker-entrypoint.sh haproxy ${HAPROXY_PARAMETERS}"
+exec /docker-entrypoint.sh haproxy ${HAPROXY_PARAMETERS}
+

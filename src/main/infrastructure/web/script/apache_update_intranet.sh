de34071924e27f1afbdf879e314c55155a4ad2fd
rvcoutinho <me@rvcoutinho.com>
2020-09-17 18:34:46 -0300

    - Setting multiple access nets;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/web/script/apache_update_intranet.sh
+++ src/main/infrastructure/web/script/apache_update_intranet.sh
@@ -41,14 +41,14 @@ trap - INT TERM
 ${DEBUG} && echo "Running 'apache_update_intranet'"
 
 CONF_FILE=/usr/local/apache2/conf/extra/httpd-intranet.conf
+CONFIG_UPDATED=false
 
 # For each host.
-CONFIG_UPDATED=false
 for HOST_NUMBER in $(seq 0 19)
 do
 
 	# Gets the old and new host IPs.
-	OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Intranet ${HOST_NUMBER}." | grep "Allow" | sed "s/^[ \t]*//g" )
+	OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Entry ${HOST_NUMBER}\." | grep "Allow" | sed "s/^[ \t]*//g" )
 	${DEBUG} && echo "OLD_HOST_CONFIG=${OLD_HOST_CONFIG}"
 
 	# If the Intranet IP is valid.
@@ -87,6 +87,60 @@ do
 	else
 		${DEBUG} && echo "Invalid intranet IP: ${INTRANET_IP}"
 	fi
+
+done
+
+# For each available net.
+for NET in $(echo ${AVAILABLE_NETS} | sed "s/,/ /g")
+do
+
+	# For each host.
+	for HOST_NUMBER in $(seq 0 19)
+	do
+	
+		# Gets the old and new host IPs.
+		CONF_FILE=/usr/local/apache2/conf/extra/httpd-access-${NET}.conf
+		OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Entry ${HOST_NUMBER}\." | grep "Allow" | sed "s/^[ \t]*//g" )
+		${DEBUG} && echo "OLD_HOST_CONFIG=${OLD_HOST_CONFIG}"
+	
+		# If the Intranet IP is valid.
+		INTRANET_IP=$( dig +short site${HOST_NUMBER}.${NET} | tail -1 )
+		if expr "${INTRANET_IP}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
+		
+			# Gets the new host configuration.		
+			NEW_HOST_CONFIG="Allow from ${INTRANET_IP}"
+			NEW_HOST_CONFIG=${NEW_HOST_CONFIG:=${OLD_HOST_CONFIG}}
+			${DEBUG} && echo "NEW_HOST_CONFIG=${NEW_HOST_CONFIG}"
+			
+			# If the old configuration is present and the new configuration is not present.
+			if (cat ${CONF_FILE} | grep "${OLD_HOST_CONFIG}") && \
+				! (cat ${CONF_FILE} | grep "${NEW_HOST_CONFIG}")
+			then
+			
+				# Replaces them in the intranet file.
+				sed -i "s/${OLD_HOST_CONFIG}/${NEW_HOST_CONFIG}/g" ${CONF_FILE}
+			
+				# If the IP has changed.
+				if [ "${OLD_HOST_CONFIG}" != "${NEW_HOST_CONFIG}" ]
+				then
+					# Sets that configuration has been updated.
+					CONFIG_UPDATED=true
+				fi
+				
+			# If the old configuration is not present.
+			else 
+				
+				# No old config is present.
+				${DEBUG} && echo "No old config present or new config already present."
+				
+			fi
+	
+		# If the Intranet IP is not valid.
+		else
+			${DEBUG} && echo "Invalid intranet IP: ${INTRANET_IP}"
+		fi
+	
+	done
 
 done
 

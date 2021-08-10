87112b4594cfe1ecdba85b34f93c8ff670860dd3
rvcoutinho <me@rvcoutinho.com>
2021-08-10 15:40:29 -0300

    - Fixing intranet;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/nginx/script/nginx_update_intranet.sh
+++ src/main/infrastructure/nginx/script/nginx_update_intranet.sh
@@ -38,57 +38,9 @@ set -o nounset
 trap - INT TERM
 
 # Print arguments if on debug mode.
-${DEBUG} && echo "Running 'apache_update_intranet'"
+${DEBUG} && echo "Running 'nginx_update_intranet'"
 
-CONF_FILE=/usr/local/apache2/conf/extra/httpd-intranet.conf
 CONFIG_UPDATED=false
-
-# For each host.
-for HOST_NUMBER in $(seq 0 49)
-do
-
-	# Gets the old and new host IPs.
-	OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Entry ${HOST_NUMBER}\." | grep "Allow" | sed "s/^[ \t]*//g" )
-	${DEBUG} && echo "OLD_HOST_CONFIG=${OLD_HOST_CONFIG}"
-
-	# If the Intranet IP is valid.
-	INTRANET_IP=$( dig +short site${HOST_NUMBER}.${INTRANET_HOST_NAME} | tail -1 )
-	if expr "${INTRANET_IP}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
-	
-		# Gets the new host configuration.		
-		NEW_HOST_CONFIG="Allow from ${INTRANET_IP}"
-		NEW_HOST_CONFIG=${NEW_HOST_CONFIG:=${OLD_HOST_CONFIG}}
-		${DEBUG} && echo "NEW_HOST_CONFIG=${NEW_HOST_CONFIG}"
-		
-		# If the old configuration is present and the new configuration is not present.
-		if (cat ${CONF_FILE} | grep "${OLD_HOST_CONFIG}" >/dev/null) && \
-			! (cat ${CONF_FILE} | grep "${NEW_HOST_CONFIG}" >/dev/null)
-		then
-		
-			# Replaces them in the intranet file.
-			sed -i "s/${OLD_HOST_CONFIG}/${NEW_HOST_CONFIG}/g" ${CONF_FILE}
-		
-			# If the IP has changed.
-			if [ "${OLD_HOST_CONFIG}" != "${NEW_HOST_CONFIG}" ]
-			then
-				# Sets that configuration has been updated.
-				CONFIG_UPDATED=true
-			fi
-			
-		# If the old configuration is not present.
-		else 
-			
-			# No old config is present.
-			${DEBUG} && echo "No old config present or new config already present."
-			
-		fi
-
-	# If the Intranet IP is not valid.
-	else
-		${DEBUG} && echo "Invalid intranet IP: ${INTRANET_IP}"
-	fi
-
-done
 
 # For each available net.
 for NET in $(echo ${AVAILABLE_NETS} | sed "s/,/ /g")
@@ -99,8 +51,8 @@ do
 	do
 	
 		# Gets the old and new host IPs.
-		CONF_FILE=/usr/local/apache2/conf/extra/httpd-access-${NET}.conf
-		OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Entry ${HOST_NUMBER}\." | grep "Allow" | sed "s/^[ \t]*//g" )
+		CONF_FILE=/etc/nginx/conf.d/access-${NET}.conf
+		OLD_HOST_CONFIG=$( cat ${CONF_FILE} | grep -A1 "Entry ${HOST_NUMBER}\." | grep "allow" | sed "s/^[ \t]*//g" )
 		${DEBUG} && echo "OLD_HOST_CONFIG=${OLD_HOST_CONFIG}"
 	
 		# If the Intranet IP is valid.
@@ -108,7 +60,7 @@ do
 		if expr "${NET_IP}" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
 		
 			# Gets the new host configuration.		
-			NEW_HOST_CONFIG="Allow from ${NET_IP}"
+			NEW_HOST_CONFIG="allow ${NET_IP}"
 			NEW_HOST_CONFIG=${NEW_HOST_CONFIG:=${OLD_HOST_CONFIG}}
 			${DEBUG} && echo "NEW_HOST_CONFIG=${NEW_HOST_CONFIG}"
 			
@@ -148,7 +100,7 @@ done
 ${DEBUG} && echo "CONFIG_UPDATED=${CONFIG_UPDATED}"
 if ${CONFIG_UPDATED}
 then
-	kill -USR1 1
+	nginx -s reload
 fi
 
 

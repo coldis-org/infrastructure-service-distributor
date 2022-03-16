b6cbd50543c59f5275db703189566b919d4cf5c5
rvcoutinho <me@rvcoutinho.com>
2022-03-16 11:58:21 -0300

    - Impriving script;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/nginx/script/nginx_add_vhost.sh
+++ src/main/infrastructure/nginx/script/nginx_add_vhost.sh
@@ -75,13 +75,8 @@ then
 	# If the config cannot be reloaded.
 	${DEBUG} && echo "Reloading config"
 	nginx_variables
-	if nginx_check_config
-	then
-		rm -f ${VHOST}.old
-	else 
-		mv ${VHOST}.old ${VHOST} || true
-		nginx_check_config
-	fi
+	nginx_check_config
+	rm -f ${VHOST}.old
 else 
 	echo "Config file '${VHOST}' has not changed. Skipping."
 fi

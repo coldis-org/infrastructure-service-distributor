9a920d08935b170b664d62fbb5565b99b8d4face
rvcoutinho <me@rvcoutinho.com>
2025-02-10 14:08:21 -0300

    - Configuring reqlimit; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/infrastructure/nginx/script/nginx_update_nets.sh
+++ src/main/infrastructure/nginx/script/nginx_update_nets.sh
@@ -63,8 +63,19 @@ NEW_NETWORKS_CONF_FILE=/tmp/networks.conf.new
 cp -f ${NETWORKS_CONF_FILE} ${OLD_NETWORKS_CONF_FILE}
 cp -f ${GENERAL_CONF_FILE_PATH}/networks.conf.default ${NEW_NETWORKS_CONF_FILE}
 
+# Prepares reqlimitzone config.
+REQLIMITZONE_CONF_FILE=${HTTP_CONF_FILE_PATH}/reqlimitzone.conf
+if [ ! -f ${REQLIMITZONE_CONF_FILE} ]
+then
+    touch ${REQLIMITZONE_CONF_FILE}
+fi
+OLD_REQLIMITZONE_CONF_FILE=/tmp/reqlimitzone.conf.old
+NEW_REQLIMITZONE_CONF_FILE=/tmp/reqlimitzone.conf.new
+cp -f ${REQLIMITZONE_CONF_FILE} ${OLD_REQLIMITZONE_CONF_FILE}
+cp -f ${HTTP_CONF_FILE_PATH}/reqlimitzone.conf.default ${NEW_REQLIMITZONE_CONF_FILE}
+
 # Prepares reqlimit config.
-REQLIMIT_CONF_FILE=${HTTP_CONF_FILE_PATH}/reqlimit.conf
+REQLIMIT_CONF_FILE=${INCLUDE_CONF_FILE_PATH}/reqlimit.conf
 if [ ! -f ${REQLIMIT_CONF_FILE} ]
 then
     touch ${REQLIMIT_CONF_FILE}
@@ -72,7 +83,7 @@ fi
 OLD_REQLIMIT_CONF_FILE=/tmp/reqlimit.conf.old
 NEW_REQLIMIT_CONF_FILE=/tmp/reqlimit.conf.new
 cp -f ${REQLIMIT_CONF_FILE} ${OLD_REQLIMIT_CONF_FILE}
-cp -f ${HTTP_CONF_FILE_PATH}/reqlimit.conf.default ${NEW_REQLIMIT_CONF_FILE}
+cp -f ${INCLUDE_CONF_FILE_PATH}/reqlimit.conf.default ${NEW_REQLIMIT_CONF_FILE}
 
 # Configures the local network.
 if [ ! -z "${LOCALNET_IPS}" ]
@@ -103,6 +114,7 @@ then
 	
 	# Adds the local network to exception list in reqlimit config.
 	sed -ie "s|\(# Removes IPs from other networks\.\)|\1\n    \"localnet\"\t\t\t\"\";|" ${NEW_NETWORKS_CONF_FILE}
+	sed -ie "s|\(# Removes IPs from local network\.\)|\1\n    \"localnet\"\t\t\t\"\";|" ${NEW_NETWORKS_CONF_FILE}
 
 else
     echo "" > ${LOCALNET_CONF_FILE}
@@ -120,7 +132,7 @@ do
 	sed -ie "s|\(# Removes IPs from other networks\.\)|\1\n    \"${NET}\"\t\t\t\"\";|g" ${NEW_NETWORKS_CONF_FILE}
 
 	# Adds the network reqlimit key.
-    sed -ie "s|\(# Defines request limit zones for specific networks\.\)|\1\nlimit_req_zone \$localnet_ip_key zone=one:10m rate=1r/s|" ${NEW_REQLIMIT_CONF_FILE} 
+    sed -ie "s|\(# Defines request limit zones for specific networks\.\)|\1\nlimit_req_zone \$localnet_ip_key zone=one:10m rate=1r/s|" ${NEW_REQLIMITZONE_CONF_FILE} 
 
 	# Creates a key for the network IP addresses
     echo "
@@ -141,9 +153,12 @@ map \$network \$remote_addr_${NET} {
 	echo "
 # Defines request limit zones for ${NET} network.
 limit_req_zone \$remote_addr_${NET} zone=${NET}_http_limit:${REQLIMIT_ZONE_SIZE} rate=${REQLIMIT_RATE};
-" >> ${NEW_REQLIMIT_CONF_FILE}
+" >> ${NEW_REQLIMITZONE_CONF_FILE}
 	
-	# Creates the old and new files.
+	# Adds the request limit for the network.
+	sed -ie "s|\(# Configures request limits\.\)|\1\nlimit_req zone=${NET}_http_limit nodelay;|" ${REQLIMIT_CONF_FILE}
+
+	# Creates the access files.
 	NET_CONF_FILE=${INCLUDE_CONF_FILE_PATH}/access-${NET}.conf
 	NET_HTTP_CONF_FILE=${INCLUDE_CONF_FILE_PATH}/access-${NET}-http.conf
 	if [ ! -f ${NET_CONF_FILE} ]
@@ -196,7 +211,7 @@ if (\$network = \"${NET}\") {
 	for HOST_NUMBER in $(seq 0 199)
 	do
 
-		# Initilize vars
+		# Initilize svars.
 		NEW_HOST_CONFIG=
 		NET_IP=
 		${DEBUG} && echo "Entry ${HOST_NUMBER}"
@@ -238,9 +253,10 @@ CONFIG_UPDATED=true
 if ( ! ${SKIP_RELOAD} ) && \
 	diff -q "${OLD_NET_CONF_FILE}" "${NEW_NET_CONF_FILE}" > /dev/null && \
 	diff -q "${OLD_NET_HTTP_CONF_FILE}" "${NEW_NET_HTTP_CONF_FILE}" > /dev/null && \
-	diff -q "${OLD_REQLIMIT_CONF_FILE}" "${NEW_REQLIMIT_CONF_FILE}" > /dev/null && \
+	diff -q "${OLD_REQLIMITZONE_CONF_FILE}" "${NEW_REQLIMITZONE_CONF_FILE}" > /dev/null && \
 	diff -q "${OLD_LOCALNET_CONF_FILE}" "${NEW_LOCALNET_CONF_FILE}" > /dev/null && \
-	diff -q "${OLD_NETWORKS_CONF_FILE}" "${NEW_NETWORKS_CONF_FILE}" > /dev/null
+	diff -q "${OLD_NETWORKS_CONF_FILE}" "${NEW_NETWORKS_CONF_FILE}" > /dev/null && \
+	diff -q "${OLD_REQLIMIT_CONF_FILE}" "${NEW_REQLIMIT_CONF_FILE}" > /dev/null
 then
     CONFIG_UPDATED=false
 fi
@@ -250,13 +266,15 @@ then
 	cp -f ${NEW_LOCALNET_CONF_FILE} ${LOCALNET_CONF_FILE}
 	cp -f ${NEW_NET_CONF_FILE} ${NET_CONF_FILE}
 	cp -f ${NEW_NET_HTTP_CONF_FILE} ${NET_HTTP_CONF_FILE}
+	cp -f ${NEW_REQLIMITZONE_CONF_FILE} ${REQLIMITZONE_CONF_FILE}
 	cp -f ${NEW_REQLIMIT_CONF_FILE} ${REQLIMIT_CONF_FILE}
 	cp -f ${NEW_NETWORKS_CONF_FILE} ${NETWORKS_CONF_FILE}
 	rm -f ${OLD_NET_CONF_FILE} ${NEW_NET_CONF_FILE} \
 		${OLD_NET_HTTP_CONF_FILE} ${NEW_NET_HTTP_CONF_FILE} \
+		${OLD_REQLIMITZONE_CONF_FILE} ${NEW_REQLIMITZONE_CONF_FILE} \
 		${OLD_REQLIMIT_CONF_FILE} ${NEW_REQLIMIT_CONF_FILE} \
 		${OLD_LOCALNET_CONF_FILE} ${NEW_LOCALNET_CONF_FILE} \
-		${OLD_NETWORKS_CONF_FILE} ${NEW_NETWORKS_CONF_FILE}
+		${OLD_NETWORKS_CONF_FILE} ${NEW_NETWORKS_CONF_FILE} 
 	nginx_variables ${SKIP_RELOAD_PARAM}
 	nginx_check_config ${SKIP_RELOAD_PARAM}
 fi
